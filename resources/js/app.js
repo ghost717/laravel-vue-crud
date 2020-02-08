5c9d813917e9c8d31a487f8dd53f612af1d971e5
ghost717 <dawid.zawadzki7717@gmail.com>
2020-02-08 15:27:29 +0100

    refactoring front-end

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,16 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import App from './components/App.vue';
+Vue.config.devtools = true;
+Vue.config.performance = true;
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+import App from './App.vue';
 
 const app = new Vue({
   el: '#app',
--- resources/js/components/CrudComponent.vue
+++ resources/js/components/CrudComponent.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="crud">
-    <div class="col-1">
-      <img :src="image"/>
+    <div class="crud">
+        <div class="col-1">
+            <img :src="image"/>
+        </div>
+        <div class="col-2">
+            <h3>Name: {{ name | properCase }}</h3>
+            <select @change="update">
+                <option
+                        v-for="col in [ 'red', 'green' ]"
+                        :value="col"
+                        :key="col"
+                        :selected="col === color ? 'selected' : ''"
+                >{{ col | properCase }}</option>
+
+            </select>
+            <button @click="del">Delete</button>
+        </div>
     </div>
-    <div class="col-2">
-      <h3>Name: {{ name | properCase }}</h3>
-      <select @change="update">
-        <option
-          v-for="col in [ 'red', 'green' ]"
-          :value="col"
-          :key="col"
-          :selected="col === color ? 'selected' : ''"
-        >{{ col | properCase }}</option>
-      </select>
-      <button @click="del">Delete</button>
-    </div>
-  </div>
 </template>
 <script>
   export default {
@@ -41,4 +42,21 @@
   }
 </script>
 <style>
+    .crud {
+        display: flex;
+        margin: 1em 1em 1em 0;
+        border: 1px solid #d1d1d1;
+        padding: 1em;
+        max-width: 350px;
+        background-color: white;
+    }
+    .crud img {
+        height: 70px;
+    }
+    .col-2 {
+        margin-left: 1em;
+    }
+    .col-2 > h3 {
+        margin: 0.5em 0;
+    }
 </style>
